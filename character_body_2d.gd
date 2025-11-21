d7a2e1e2ad0b4f77baa7af68bcb79fa79ddf51ad
d51023 <d51023@outlook.fr>
2025-11-21 12:47:19 -0500

    first commit

---
+++ character_body_2d.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(_delta: float) -> void:
+
+	var mouse_pos = get_global_mouse_position()
+	
+	var input_vector := Vector2.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		input_vector.x += 1
+	if Input.is_action_pressed("move_left"):
+		input_vector.x -= 1
+	if Input.is_action_pressed("move_down"):
+		input_vector.y += 1
+	if Input.is_action_pressed("move_up"):
+		input_vector.y -= 1
+
+	input_vector = input_vector.normalized()
+
+	velocity = input_vector * SPEED
+	move_and_slide()
+	
+	if(abs(mouse_pos.x - global_position.x) > 1 && abs(mouse_pos.y - global_position.y) > 1):
+		look_at(mouse_pos)
+		
+	
